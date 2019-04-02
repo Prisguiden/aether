16827602a36bfe26c4f08793ecbbf6c9fbb7ac08
Simen Graaten <simen@prisguiden.no>
2019-04-02 12:58:53 +0200

    Remove old timer hack

--- src/Response/Text.php
+++ src/Response/Text.php
@@ -45,53 +45,8 @@ class Text extends Response
             $_SESSION['wasGoingTo'] = $_SERVER['REQUEST_URI'];
         }
 
-        $tpl = $sl->getTemplate();
+        $out = $this->out;
 
-        $timer = $sl->get('timer');
-
-        if ($timer && $tpl->templateExists('debugBar.tpl')) {
-            // Timer
-            $timer->end('aether_main');
-            // Replace into out content
-            //$tpl->selectTemplate('debugBar');
-            $timers = $timer->all();
-            foreach ($timers as $key => $tr) {
-                foreach ($tr as $k => $t) {
-                    if (!array_key_exists('elapsed', $t)) {
-                        $t['elapsed'] = 0;
-                    }
-                    $timers[$key][$k]['elapsed'] = number_format($t['elapsed'], 4);
-                    // Format memory
-                    if (isset($t['memory'])) {
-                        $memLen = strlen($t['memory']);
-                        $memUse = $t['memory'];
-                        if ($memLen > 9) {
-                            $memUse = round($memUse / (1000*1000*1000), 1) . "GB";
-                        }
-                        if ($memLen > 6) {
-                            $memUse = round($memUse / (1000*1000), 1) . "MB";
-                        } elseif ($memLen > 3) {
-                            $memUse = round($memUse / (1000), 1) . "KB";
-                        }
-                        $timers[$key][$k]['mem_use'] = $memUse;
-                    }
-                }
-            }
-            $tpl->set('timers', $timers);
-            try {
-                $out = $tpl->fetch('debugBar.tpl');
-            } catch (Exception $e) {
-                echo $e->getMessage();
-            }
-            $out = str_replace(
-                "<!--INSERTIONPOINT-->",
-                $out,
-                $this->out
-            );
-        } else {
-            // No timing, we're in prod
-            $out = $this->out;
-        }
         if ($this->contentType) {
             header("Content-Type: {$this->contentType}; charset=UTF-8");
         }
